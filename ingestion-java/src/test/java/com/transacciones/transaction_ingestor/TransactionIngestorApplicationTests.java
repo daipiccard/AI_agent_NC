6ccf8a7110fd9d0e1360e3489874eeaae2c6dcfd
Ariel Plaza <arielplazasalinas@gmail.com>
2025-10-22 22:44:23 -0300

    prueba  y mas prueba

--- ingestion-java/src/test/java/com/transacciones/transaction_ingestor/TransactionIngestorApplicationTests.java
+++ ingestion-java/src/test/java/com/transacciones/transaction_ingestor/TransactionIngestorApplicationTests.java
@@ -2,15 +2,10 @@ package com.transacciones.transaction_ingestor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.context.EmbeddedKafka;  // <- IMPORT NECESARIO
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-@EmbeddedKafka(partitions = 1, topics = { "transactions-topic-test" }, brokerProperties = {
-    "listeners=PLAINTEXT://localhost:9093",
-    "port=9093"
-})
+@ActiveProfiles("test")  // <-- ESTO DEBE ESTAR
 class TransactionIngestorApplicationTests {
 
 	@Test
@@ -18,5 +13,3 @@ class TransactionIngestorApplicationTests {
 	}
 
 }
-
-
