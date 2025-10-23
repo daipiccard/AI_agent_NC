53f7656706fe80aaaeadc0019bb16cb974dc9545
Ariel Plaza <arielplazasalinas@gmail.com>
2025-10-22 22:56:47 -0300

    feature: test con kafka exitoso

--- ingestion-java/src/test/java/com/transacciones/transaction_ingestor/TransactionIngestorApplicationTests.java
+++ ingestion-java/src/test/java/com/transacciones/transaction_ingestor/TransactionIngestorApplicationTests.java
@@ -2,10 +2,12 @@ package com.transacciones.transaction_ingestor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")  // <-- ESTO DEBE ESTAR
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 class TransactionIngestorApplicationTests {
 
 	@Test
